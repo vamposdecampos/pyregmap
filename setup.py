6035dcd7ff582c697815d890a8f0365568990fa0
Alex Badea <vamposdecampos@gmail.com>
2016-03-26 17:01:17 +0200

    build: add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = 'regmap',
+	version = '0.0.1',
+	description = 'Register map frobber',
+	author = 'Alex Badea',
+	author_email = 'vamposdecampos@gmail.com',
+	url = 'https://github.com/vamposdecampos/pyregmap',
+	license = 'MIT',
+	packages = find_packages(),
+)
